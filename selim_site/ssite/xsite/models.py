d134b01824287d501e83660f3c6cd185820bb4e4
kayrahancoskun <safa.selim00@gmail.com>
2024-10-09 17:22:04 +0300

    init

--- selim_site/ssite/xsite/models.py
+++ selim_site/ssite/xsite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class xsite(models.Model):
+    # model alanları
+    pass
+
+class Category(models.Model):
+    # model alanları
+    pass
